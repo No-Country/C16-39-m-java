f07dfc235cf9d90cb8c52c1cdc646d844b960ac8
Hiram Chavez <virtual.liga@gmail.com>
2024-02-15 16:54:11 -0600

    Creating example welcome endpoint

--- backend/src/main/java/com/c1639/backend/service/UserService.java
+++ backend/src/main/java/com/c1639/backend/service/UserService.java
@@ -1,4 +1,13 @@
 package com.c1639.backend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    public String welcome() {
+        return "Welcome to the User Service";
+    }
 }
