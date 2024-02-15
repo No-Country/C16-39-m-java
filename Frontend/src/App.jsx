ee7141fed4fb7d0aa2dc6f08d8e112553ba422ec
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2024-02-15 14:55:56 -0300

    Intalacion de react-router-dom, creacion de carpetas components y pages, inicializacion de ruta en app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,18 @@
 
-
+import {Routes, Route} from "react-router-dom"
+import Login from "./pages/Login/Login"
 function App() {
 	
 	return (
 		<>
-			<h1>Hola</h1>
+			
+
+			<Routes>
+
+				<Route path="/login" element= {<Login/>}/>
+			</Routes>
 		</>
-	)
+	);
 }
 
 export default App
