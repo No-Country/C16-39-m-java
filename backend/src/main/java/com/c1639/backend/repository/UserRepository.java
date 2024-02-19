55a310de153ec8e71d27b867e8325eb0bccb570a
Hiram Chavez <virtual.liga@gmail.com>
2024-02-19 07:12:44 -0600

    Adding MapStruct dependency and UserRepository created

--- backend/src/main/java/com/c1639/backend/repository/UserRepository.java
+++ backend/src/main/java/com/c1639/backend/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.c1639.backend.repository;
 
-public class UserRepository {
+import com.c1639.backend.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
