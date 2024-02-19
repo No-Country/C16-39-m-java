afd572e27a9c585938cd4b4ca782cae0cb3e3977
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2024-02-19 15:25:59 -0300

    login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import {Routes, Route, BrowserRouter} from "react-router-dom"
-// import Login from "./pages/Login/Login"
+import {Routes, Route} from "react-router-dom"
+import Login from "./pages/Login/Login"
 import SignUp from "./pages/SignUp/SignUp";
 
 
 function App() {
 	
 	return (
-		<BrowserRouter>
+	
 			<Routes>
-				{/* <Route path="/login" element= {<Login/>}/> */}
+				<Route path="/login" element= {<Login/>}/>
 				<Route path="/sign-up" element= {<SignUp />}/>
 			</Routes>
-		</BrowserRouter>
+	
 	);
 }
 
