dbbbc2723e31d7a0e069279d451236232da9fbc6
Bernardo2193 <Royo2193@gmail.com>
2024-02-20 12:30:42 -0300

    Model Comment in progress

--- backend/src/main/java/com/c1639/backend/model/Comment.java
+++ backend/src/main/java/com/c1639/backend/model/Comment.java
@@ -1,23 +1,24 @@
 package com.c1639.backend.model;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.persistence.*;
 
-
-
+@Entity
+@Table(name = "Comment")
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Comment {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-
-
-
+    private Integer review;
 
 
     public Comment(Long id) {
