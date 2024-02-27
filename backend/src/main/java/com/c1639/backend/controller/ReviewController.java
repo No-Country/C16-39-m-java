8f47344f2454edc2bc021e36f41a15209ba68277
Melina Martinez <martinezmelinadanmail.com>
2024-02-27 14:35:12 -0300

    avances controller

--- backend/src/main/java/com/c1639/backend/controller/ReviewController.java
+++ backend/src/main/java/com/c1639/backend/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.c1639.backend.controller;
 
 import com.c1639.backend.dto.review.ListReviewsDTO;
+import com.c1639.backend.dto.review.ReviewDTO;
 import com.c1639.backend.model.review.Review;
 import com.c1639.backend.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,9 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,5 +46,67 @@ public class ReviewController {
     public ResponseEntity<List<Review>> getAlReviews(){
         List<Review> reviews = reviewService.getAllReviews();
         return ResponseEntity.ok(reviews);
+    }
+
+    @Operation(
+            summary = "Create a review",
+            description = "Add the review to the database"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Review created successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ListReviewsDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Cannot create a review", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = {@Content}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content})
+    })
+    @PostMapping("/new")
+    public Review createReview(Review review) {
+        return reviewService.createReview(review);
+    }
+
+    @Operation(
+            summary = "Update a review",
+            description = "Change the rate of a review"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Review updated successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ReviewDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Cannot update the review", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = {@Content}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content})
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<Review> updateReview(@PathVariable Long id, Review updatedReview) {
+        Review review = reviewService.updateReview(id, updatedReview);
+        return ResponseEntity.ok(review);
+    }
+
+    @Operation(
+            summary = "Get review",
+            description = "Find a review using their ID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+            responseCode = "200", description = "Review returned successfully",
+            content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ReviewDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Cannot return the movie", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = {@Content}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content})
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Review> getReviewById(@PathVariable Long id){
+        Review review = reviewService.getById(id);
+        return ResponseEntity.ok(review);
     }
 }
