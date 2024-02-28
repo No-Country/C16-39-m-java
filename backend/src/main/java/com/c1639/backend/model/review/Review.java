778264ad35da9925d41cd2f63a007e0a312e4bdb
Melina Martinez <martinezmelinadanmail.com>
2024-02-28 14:46:47 -0300

    relaciones de review con las entidades use, comment y movie

--- backend/src/main/java/com/c1639/backend/model/review/Review.java
+++ backend/src/main/java/com/c1639/backend/model/review/Review.java
@@ -3,6 +3,8 @@ package com.c1639.backend.model.review;
 import com.c1639.backend.model.comment.Comment;
 import com.c1639.backend.model.movie.Movie;
 import com.c1639.backend.model.user.User;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,13 +19,21 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @Builder
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class, property="id")
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", referencedColumnName = "id")
     private Movie movie;
     private Integer rate;
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
     private Comment comment;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
