8851a9a2d38baa8ae9bf279cacafea909cdb605f
francojnieva <franco97nieva@hotmail.com>
2024-03-04 21:42:10 -0300

    Add a component to handle private routes and a message when there are no favorite movies.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login/Login"
 import SignUp from "./pages/SignUp/SignUp";
 import Home from "./pages/Home/Home";
@@ -6,25 +6,27 @@ import DetallesPeliculas from "./pages/Detalle/DetallesPeliculas"
 import PaginaNoEncontrada from "./pages/PaginaNoEncontrada/PaginaNoEncontrada"
 import TopRanking from "./pages/TopRanking/TopRanking";
 import LayoutDashboard from "./components/LayoutDashboard/LayoutDashboard";
-import { AuthContextProvider } from "./context/AuthContext/AuthContext";
 import Favorite from "./pages/Favorite/Favorite";
+import { AuthContextProvider } from "./context/AuthContext/AuthContext";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute ";
 
 
 function App() {
 	
 	return (
-			<AuthContextProvider>
-				<Routes>
-					<Route path="/" element= {<Login/>}/>
-					<Route path="/sign-up" element= {<SignUp />}/>
-					<Route path='/Detalle/:id' element= {<LayoutDashboard><DetallesPeliculas/></LayoutDashboard>}/>
-					<Route path="*" element= {<PaginaNoEncontrada/>}/>
-					<Route path ="/dashboard" element= {<LayoutDashboard><Home/></LayoutDashboard>}/>
-					<Route path ="/top-ranking" element= {<LayoutDashboard><TopRanking/></LayoutDashboard>}/>
-					<Route path ="/favorite" element= {<LayoutDashboard><Favorite/></LayoutDashboard>}/>
-				</Routes>
-			</AuthContextProvider>
-	
+		<AuthContextProvider>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="*" element={<PaginaNoEncontrada />} />
+				<Route element={<PrivateRoute />}>
+					<Route path="/dashboard" element={<LayoutDashboard><Home /></LayoutDashboard>} />
+					<Route path="/top-ranking" element={<LayoutDashboard><TopRanking /></LayoutDashboard>} />
+					<Route path="/Detalle/:id" element={<LayoutDashboard><DetallesPeliculas /></LayoutDashboard>} />
+					<Route path="/favorite" element={<LayoutDashboard><Favorite /></LayoutDashboard>} />
+				</Route>
+			</Routes>
+		</AuthContextProvider>
 	);
 }
 
